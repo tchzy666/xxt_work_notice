de16aeb78a72ff58619db3a9ef4ca28361483e19
H0ng <94291935+tchzy666@users.noreply.github.com>
2025-01-03 15:13:54 +0800

    Update xxt_Homework_Notice.py
    关于：
    res = req.get(url)
    if res.json()['mes'] == "验证通过":
    的问题，新的设备在登录时需要进行验证码验证，在json中并没有验证成功的字节，改变浏览器标识头即可绕过验证"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"

--- xxt_Homework_Notice.py
+++ xxt_Homework_Notice.py
@@ -8,7 +8,7 @@ req = requests.Session()
 req.headers = {
     "Accept-Encoding": "gzip",
     "Accept-Language": "zh-Hans-CN;q=1, zh-Hant-CN;q=0.9",
-    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_2_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 com.ssreader.ChaoXingStudy/ChaoXingStudy_3_4.8_ios_phone_202012052220_56 (@Kalimdor)_12787186548451577248"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"
 }
 
 def get_course():
